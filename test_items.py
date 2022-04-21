d5c6091c56e956e8ba4ffc32291cd40445fb2d60
Vera Kozlova <kozlovera@tt.tt>
2022-04-20 22:29:16 -0700

    initial commit

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_addToBasketButton(browser):
+    browser.get(link)
+    time.sleep(10)
+    browser.find_element_by_css_selector("button.btn")
