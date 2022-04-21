ef03f9c8cb6da93a03738dbc99eaa3755d7fe648
Vera Kozlova <kozlovera@tt.tt>
2022-04-21 13:43:55 -0700

    second commit

--- test_items.py
+++ test_items.py
@@ -5,4 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_addToBasketButton(browser):
     browser.get(link)
     time.sleep(10)
-    browser.find_element_by_css_selector("button.btn")
+    basket = browser.find_elements_by_css_selector("button.btn")
+    assert basket, "Button not found"
